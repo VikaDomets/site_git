a4b9cb72d275040ba1ac33e91bff9fcca2d3e6bd
VikaDomets <116880735+VikaDomets@users.noreply.github.com>
2025-04-29 23:34:15 +0300

    Delete Frontend directory

--- Frontend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from '../context/AuthContext';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PrivateRoute = () => {
-    const { user } = useAuth();
-    return user ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
